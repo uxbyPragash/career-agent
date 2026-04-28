6d4ac332b5f25fd9a3667923fc436e4196c37cbd
uxbyPragash <pragashux@outlook.com>
2026-04-28 12:23:59 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // Allow CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -20,10 +19,8 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body)
     });
-
     const data = await response.json();
     res.status(200).json(data);
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
